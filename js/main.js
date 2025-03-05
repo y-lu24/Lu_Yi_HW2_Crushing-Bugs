3a37ebfc303eb00d26822e060120868a15712586
y-lu24 <yilu2559@gmail.com>
2025-03-04 20:45:29 -0500

    Update main.js
    In this commit, updated the main.js file.

--- js/main.js
+++ js/main.js
@@ -1,42 +1,23 @@
-// variables always go at the top -> this is step 1
-// these are the connections that you're making to elements on the page
-// use CSS selectors to make connections to elements with JavaScript
-
-// create a 1 to 1 connection with a variable -> querySelector("queryString")
-// let theButton = document.querySelector("#buttonOne");
-
-// create a 1 to many connection with a variable -> querySelectorAll("queryString")
 let theButtons = document.querySelectorAll('#buttonHolder img'),
 puzzleBoard = document.querySelector('.puzzle-board'),
 puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
 dropZones = document.querySelectorAll('.drop-zone'),
-	// store the dragged piece in a global variable
-	// because we need it in the handleDrop function
+
     draggedPiece;
 
-// step 3
-// functionality always goes in the middle -> how do we want
-// the app to behave?
-function changeBGImage() {
-	// the `` is a JavaScript template string. It tells the JS enging to evaluate the expression
-	// inside the braces - run that little bit of code. In this case it's just pulling the ID of the
-	// button we clicked on and putting it at the end of the image name (0, 1, 2, 3)
-	// and updating the background-image style of the puzzle board element.
 
-	// bug fix #2 should go here. it's at most 3 lines of JS code.
+function changeBGImage() {
+	
     puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
 }
 
 function handleStartDrag() {
     console.log('started dragging this piece: ', this);
-    // store a reference to the puzzle piece image that we're dragging
-	// so we can use it later and move it to a drop zone
     draggedPiece = this;
 }
 
 function handleDragOver(e) {
-    e.preventDefault(); // e is shorthand for event
-	// this overrides the default dragover behaviour
+    e.preventDefault(); 
     console.log('you dragged over me');
 }
 
